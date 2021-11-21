aa2109b4754886d12b74c4f9b8a61833b18951c9
Mike Gysel <z5251938@ad.unsw.edu.au>
2021-11-21 18:51:33 +0100

    Tested

--- template_java/src/main/java/cs451/BestEffortBroadcast.java
+++ template_java/src/main/java/cs451/BestEffortBroadcast.java
@@ -1,42 +1,23 @@
 package cs451;
 
 import java.util.List;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class BestEffortBroadcast extends Thread implements MyEventListener {
     private PerfectLinks pl;
-    // private String output;
-    // private final ReentrantReadWriteLock outputLock = new ReentrantReadWriteLock();
     
     public BestEffortBroadcast(PerfectLinks pl) {
         this.pl = pl;
-        this.pl.setMyEventListener(this);
+        // this.pl.setMyEventListener(this);
     }
 
     // Broadcast
-    public void broadcastAll() {
-        pl.sendAll();
+    public void broadcast(Message m) {
+        // For all peers, pl.send(pi, m)
+        List<Host> hosts = pl.getHosts().getHosts();
+        for (Host host: hosts) {
+            pl.send(host, m);
+        }
     }
-
-    // // Broadcast
-    // public void broadcast(String m) {
-    //     // For all peers, pl.send(pi, m)
-    //     List<Host> peers = pl.getPeers();
-    //     for (Host peer: peers) {
-    //         pl.send(peer.getId(), m);
-    //     }
-    //     writeBroadcast(m);
-    // }
-
-    // // Run server
-    // public void run() {
-    //     pl.start();
-    // }
-
-    // // Return output
-    // public String close() {
-    //     return pl.close();
-    // }
 
     @Override
     public void PerfectLinksDeliver(Host p, Message m) {
@@ -48,20 +29,4 @@ public class BestEffortBroadcast extends Thread implements MyEventListener {
     public void ReceivedAck(String m) {
         
     }
-
-    // private void deliver(int p, String m) {
-    //     writeDeliver(p, m);
-    // }
-
-    // private void writeDeliver(int p, String m) {
-    //     outputLock.writeLock().lock();
-    //     output = String.format("%sd %s %s\n", output, p, m);
-    //     outputLock.writeLock().unlock();
-    // }
-
-    // private void writeBroadcast(String m) {
-    //     outputLock.writeLock().lock();
-    //     output = String.format("%sb %s\n", output, m);
-    //     outputLock.writeLock().unlock();
-    // }
 }
